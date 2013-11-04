7a0d3f62f6c1675573c6026034e11c3934a914f4
Shockk <shokku.ra@gmail.com>
2013-11-04 23:18:59 +0000

    Modified evaluation procedure.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var parser = require('./parser')
 var fs = require('fs')
+var util = require('util')
+
+var evaluate = function(o)
+{
+    return o;
+}
 
 fs.readFile('test.shk', function(err, data)
 {
     if(err) throw err
-    console.log(parser.parse(data.toString()))
+    var parsed = parser.parse(data.toString())
+
+    parsed.forEach(function(v, k, a)
+    {
+        console.log(evaluate(v))
+    })
 })
