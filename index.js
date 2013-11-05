a64fdf7ee53aaa4b097c0b3a3f8ad3cd31c638fb
Shockk <shokku.ra@gmail.com>
2013-11-05 00:56:29 +0000

    Refactored evaluate function to show fancy output.

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var util = require('util')
 
 var evaluate = function(o)
 {
-    return o;
+    if(typeof o !== 'object') return ' ' + o.toString()
+
+    var s = '\n' + o[0]
+
+    for(var i=1; i<o.length; ++i)
+    {
+        s += evaluate(o[i]).replace(/^/gm, '  ')
+    }
+
+    return s
 }
 
 fs.readFile('test.shk', function(err, data)
